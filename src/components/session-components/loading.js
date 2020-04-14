27da183a1cb4d554258115aee6ea571121000b94
David B <david_beale@outlook.com>
2020-04-14 17:20:43 +0100

    add loading page

--- src/components/session-components/loading.js
+++ src/components/session-components/loading.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
+import Loader from 'react-loader-spinner'
+import '../../styles/session-styles/loading.css'
 
 function Loading () {
   return (
-    <div className="row mt-5">
+    <div className="loading">
+      <Loader
+        type="Rings"
+        color="#00BFFF"
+        height={400}
+        width={400}
+        timeout={0}
+      />
+      Loading. Please Wait...
     </div>
   )
 }
